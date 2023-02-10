dcb00fa4438d1711355e85ba50bb5ed356fbc070
Victor Hogeweij <vo.hogeweij@student.han.nl>
2023-02-10 21:09:51 +0100

    -add project files for the desktop testing application

---
+++ Desktop_application/Flow_and_ToF_Testsuite/src/main/java/com/babypatients/babysim_testapp/Babysim_testapp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.babypatients.babysim_testapp;
+
+/**
+ *
+ * @author victor
+ */
+public class Babysim_testapp {
+
+    public static void main(String[] args) {
+          new main_panel().setVisible(true);
+    }
+}
---
+++ Desktop_application/Flow_and_ToF_Testsuite/target/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.babypatients.babysim_testapp","l":"Babysim_testapp"},{"p":"com.babypatients.babysim_testapp","l":"main_panel"}];updateSearchResults();
