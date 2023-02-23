87ea1e48976d5ddee72ac56b5f17ecee674fe247
Victor Hogeweij <vo.hogeweij@student.han.nl>
2023-02-23 10:57:50 +0100

    -enh forgot renaming private members in sensor_compression class

--- SensorHub_FW/lib/Sensor/include/sensors/sensor_compression.hpp
+++ SensorHub_FW/lib/Sensor/include/sensors/sensor_compression.hpp
@@ -8,8 +8,8 @@ class CompressionSensor : public UniversalSensor {
  public:
   explicit CompressionSensor(i2c_peripheral_t i2c_peripheral)
                               : UniversalSensor(i2c_peripheral) {
-    i2c_handle = new I2CDriver(i2c_peripheral, i2c_speed_400KHz, kSensorI2CAddress_);
-    Tof = new RobotPatient_VL6180x(i2c_handle);
+    i2c_handle_ = new I2CDriver(i2c_peripheral, i2c_speed_400KHz, kSensorI2CAddress_);
+    Tof_ = new RobotPatient_VL6180x(i2c_handle_);
   }
   void Initialize() override;
   SensorData GetSensorData() override;
@@ -20,7 +20,7 @@ class CompressionSensor : public UniversalSensor {
  private:
   const uint8_t kSensorI2CAddress_ = 0x29;
   SensorData sensor_data_ {};
-  I2CDriver* i2c_handle;
-  RobotPatient_VL6180x *Tof;
+  I2CDriver* i2c_handle_;
+  RobotPatient_VL6180x *Tof_;
 };
 #endif  // SENSOR_COMPRESSION_HPP
