fa91f75e71868341546a4efd280ff77ec4e71a45
Victor Hogeweij <vo.hogeweij@student.han.nl>
2023-03-07 16:39:48 +0100

    -enh improve main.cpp to use new sensorlib api

--- SensorHub_FW/src/main.cpp
+++ SensorHub_FW/src/main.cpp
@@ -6,12 +6,17 @@
 /* Create handle for sensor connector PORT_A.
  * This will be used in setup() and loop()*/
 UniversalSensor *connector_port_a;
-I2CDriver i2c_handle_port_a = I2CDriver(&wireSensorA, ki2cSpeed_400KHz);
+UniversalSensor *connector_port_b;
+I2CDriver i2c_handle_port_a = I2CDriver(&wireSensorA, ki2cSpeed_100KHz);
+I2CDriver i2c_handle_port_b = I2CDriver(&wireSensorB, ki2cSpeed_100KHz);
 
-void printResults(SensorData data){
+void printResults(String Prefix, SensorData data){
   for(int i =0; i< data.numOfBytes/2; i++) {
-    Serial.print("R:");
+    Serial.print(Prefix+"R:");
     Serial.print(data.buffer[i]);
+  }
+  if(data.numOfBytes == 1){
+    Serial.print(data.buffer[0]);
   }
   Serial.print("\n");
 
@@ -24,17 +29,21 @@ void setup() {
   while (!Serial)
     continue;
   wireBackbone.begin();
-  wireSensorA.begin();
-  wireSensorB.begin();
   InitI2CPins();
-  connector_port_a = new CompressionSensor(&i2c_handle_port_a);
-  
+  i2c_handle_port_a.init();
+  i2c_handle_port_b.init();
+  connector_port_a = new FingerPositionSensor(&i2c_handle_port_a);
+  connector_port_a->Initialize();
+  connector_port_b = new DifferentialPressureSensor(&i2c_handle_port_a);
+  connector_port_b->Initialize();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  SensorData result = connector_port_a->GetSensorData();
-  printResults(result);
+  SensorData ResultSensA = connector_port_a->GetSensorData();
+  printResults("A_",ResultSensA);
+  SensorData ResultSensB = connector_port_b->GetSensorData();
+  printResults("B_",ResultSensB);
   delay(100);
 }
 
