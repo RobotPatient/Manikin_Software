0560ae173a54d19b55026f9f6cd30fd658c184ae
Victor Hogeweij <vo.hogeweij@student.han.nl>
2023-04-27 11:16:49 +0200

    -add support for new logger system

--- SensorHub_FW/src/main.cpp
+++ SensorHub_FW/src/main.cpp
@@ -3,12 +3,14 @@
 
 #include <sensorhub_settings.hpp>
 #include <sensor_helper.hpp>
-/* Create handle for sensor connector PORT_A.
- * This will be used in setup() and loop()*/
+#include <hal_exception.hpp>
+
+using namespace hal::exception;
+using namespace hal::log;
+/* Create handle for sensor connector PORT_A and PORT_B.
+ * These will be used in setup() and loop()*/
 UniversalSensor *connector_port_a;
 UniversalSensor *connector_port_b;
-I2CDriver i2c_handle_port_a = I2CDriver(&wireSensorA, kI2cSpeed_100KHz);
-I2CDriver i2c_handle_port_b = I2CDriver(&wireSensorB, kI2cSpeed_100KHz);
 
 void printResults(String Prefix, SensorData data){
   for(int i =0; i< data.num_of_bytes/2; i++) {
@@ -22,29 +24,39 @@ void printResults(String Prefix, SensorData data){
 
 }
 
+static Logger* log_inst;
+static LoggerSettings Log_module_settings;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
   while (!Serial)
     continue;
-  wireBackbone.begin();
-  InitI2CPins();
-  i2c_handle_port_a.Init();
-  i2c_handle_port_b.Init();
-  connector_port_a = new FingerPositionSensor(&i2c_handle_port_a);
-  connector_port_a->Initialize();
-  connector_port_b = new DifferentialPressureSensor(&i2c_handle_port_b);
-  connector_port_b->Initialize();
+  // wireBackbone.begin();
+  // InitI2CPins();
+  // i2c_handle_port_a.Init();
+  // i2c_handle_port_b.Init();
+  // connector_port_a = new FingerPositionSensor(&i2c_handle_port_a);
+  // connector_port_a->Initialize();
+  // connector_port_b = new DifferentialPressureSensor(&i2c_handle_port_b);
+  // connector_port_b->Initialize();
+  hal::exception::Init();
+  InitSerialLogger(log_inst, &Log_module_settings, &Serial);
+  hal::exception::attachLogger(log_inst);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  SensorData ResultSensA = connector_port_a->GetSensorData();
-  printResults("A_",ResultSensA);
-  SensorData ResultSensB = connector_port_b->GetSensorData();
-  printResults("B_",ResultSensB);
-  delay(100);
+  // SensorData ResultSensA = connector_port_a->GetSensorData();
+  // printResults("A_",ResultSensA);
+  // SensorData ResultSensB = connector_port_b->GetSensorData();
+  // printResults("B_",ResultSensB);
+  // delay(100);;
+  THROW_RESET("TEST!", UNINITIALIZED);
+  //const uint8_t i = 0;
+  //ASSERT_WARN(i != 0);
+  delay(1000);
 }
 
 #else
--- SensorHub_FW/src/sensorhub_settings.hpp
+++ SensorHub_FW/src/sensorhub_settings.hpp
@@ -2,8 +2,16 @@
 #define SENSORHUB_FW_SRC_SENSORHUB_SETTINGS_HPP_
 
 #ifdef __arm__
+#include <Arduino.h>
 #include "wiring_private.h"
 #include "Wire.h"
+#include <SPI.h>
+#include <SdFat.h>
+#include <Adafruit_SPIFlash.h>
+#include <i2c_helper.hpp>
+#include <hal_log.hpp>
+
+using namespace hal::log;
 
 // i2c system bus
 const uint8_t kW0_SCL = 27;  // PA22
@@ -19,6 +27,17 @@ TwoWire wireBackbone(&sercom3, kW0_SDA, kW0_SCL);  // Main
 TwoWire wireSensorA(&sercom1, kW1_SDA, kW1_SCL);  // Sensor A
 TwoWire wireSensorB(&sercom4, kW2_SDA, kW2_SCL);  // Sensor B
 
+I2CDriver i2c_handle_port_a = I2CDriver(&wireSensorA, kI2cSpeed_100KHz);
+I2CDriver i2c_handle_port_b = I2CDriver(&wireSensorB, kI2cSpeed_100KHz);
+
+inline constexpr uint8_t kSpiFramMisoPin = 2;
+inline constexpr uint8_t kSpiFramMosiPin = 4;
+inline constexpr uint8_t kSpiFramClkPin = 3;
+inline constexpr uint8_t kSpiFramSSPin = 9;
+
+Adafruit_FlashTransport_SPI flashTransport(kSpiFramSSPin, &SPI1);
+Adafruit_SPIFlash flash(&flashTransport);
+
 
 void InitI2CPins() {
   pinPeripheral(kW0_SDA, PIO_SERCOM);
@@ -30,5 +49,54 @@ void InitI2CPins() {
   pinPeripheral(kW1_SDA, PIO_SERCOM_ALT);
   pinPeripheral(kW1_SCL, PIO_SERCOM_ALT);
 }
+
+void InitSerialLogger(Logger* log_inst, LoggerSettings *log_settings, Serial_* serial_obj) {
+  log_settings->CommHandle.SerialHandle = serial_obj;
+  log_settings->CommMethod = communicationMethod::Serial;
+  log_inst = new SerialLogger(log_settings);
+}
+
+FatVolume fatfs;
+
+static const SPIFlash_Device_t my_flash_devices[] = {
+    MB85RS2MTA
+};
+
+static char FlashLoggerFilepath[kMaxFilePathSize];
+const char* kLoggerFilePathPrefix = "/LOG/";
+
+void InitFlashLogger(Logger* log_inst, LoggerSettings *log_settings, const char* filename) {
+   if (!flash.begin(my_flash_devices, 1)) {
+    Serial.println(F("Error, failed to initialize flash chip!"));
+    while(1) yield();
+  }
+  
+  if (!fatfs.begin(&flash)) {
+    Serial.println(F("Error, failed to mount newly formatted filesystem!"));
+    Serial.println(F("Was the flash chip formatted with the SdFat_format example?"));
+    while(1) yield();
+  };
+
+  if (!fatfs.exists("/LOG")) {
+    Serial.println(F("LOG directory not found, creating..."));
+    fatfs.mkdir("/LOG");
+  }
+
+  strcat(FlashLoggerFilepath, kLoggerFilePathPrefix);
+  strcat(FlashLoggerFilepath, filename);
+  log_settings->CommHandle.FlashHandle.FatHandle = &fatfs;
+  log_settings->CommHandle.FlashHandle.FilePath = FlashLoggerFilepath;
+  log_settings->CommMethod = communicationMethod::Flash;
+  log_inst = new FlashLogger(log_settings);
+  log_inst->init();
+  // logflash->init();
+  // logflash->setcursorpos(0);
+  // File32* flash_handle = logflash->getnativehandle().FlashHandle;
+
+  // uint32_t filesize = flash_handle->size();
+  // Serial.printf("Size: %d \n",filesize);
+}
+
+
 #endif
 #endif  // SENSORHUB_FW_SRC_SENSORHUB_SETTINGS_HPP_
