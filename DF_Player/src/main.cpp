5253566ce79653206e0ce58b3e5f8aa3f7f5a43b
zainSabbagh <99176156+zainSabbagh@users.noreply.github.com>
2023-06-09 20:19:30 +0200

    -enh added a test

--- DF_Player/src/main.cpp
+++ DF_Player/src/main.cpp
@@ -15,16 +15,24 @@ void setup() {
   else 
     Serial.println("Serial error");
 
-  myDfPlayer.dfPlayerSetVolume(4);
-
+  myDfPlayer.dfPlayerSetVolume(10);
+  myDfPlayer.dfPlayerPlay(2);
+  delay(6000);
+  myDfPlayer.dfPlayerPlay(3);
+  delay(6000);
+  myDfPlayer.dfPlayerPlay(4);
+  delay(6000);
+  myDfPlayer.dfPlayerPause();
+  delay(6000);
+  myDfPlayer.dfPlayerSetVolume(23);
+  myDfPlayer.dfPlayerResume();
+  delay(6000);
+  myDfPlayer.dfPlayerSetVolume(14);
+  myDfPlayer.dfPlayerPlayNext();
+  delay(6000);
+  myDfPlayer.dfPlayerPlayPrevious();
   
 
-//  myDfPlayer.dfPlayerPlay(2);
-  //delay(6000);
-  //myDfPlayer.dfPlayerPlay(3);
-  //delay(6000);
-  //myDfPlayer.dfPlayerPlay(4);
- // myDfPlayer.dfPlayerSetVolume(0);
 
 }
 
