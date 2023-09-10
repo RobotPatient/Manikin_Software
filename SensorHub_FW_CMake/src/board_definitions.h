bd0f52384cba6188480490b0141d791f3cbd4cdd
Victor Hogeweij <hogeweyv@gmail.com>
2023-09-10 20:26:42 +0200

    -add i2c driver example code, -enh ram usage

--- SensorHub_FW_CMake/src/board_definitions.h
+++ SensorHub_FW_CMake/src/board_definitions.h
@@ -9,5 +9,5 @@
 const GPIOPin LedPin = {kGPIOPortA, 2};
 const GPIOPin kBackBoneSDA = {kGPIOPortA, 12};
 const GPIOPin kBackBoneSCL = {kGPIOPortA, 13};
-const I2CInst I2CPeriph = {SERCOM_NUM_4, SERCOM4, CLKGEN_3, CLKGEN_0, I2COpModeSlave, 0x54};
+const I2CInst I2CPeriph = {SERCOM_NUM_4, SERCOM4, CLKGEN_3, CLKGEN_0, I2COpModeMaster, 0x54};
 #endif //ATMELSAMD21_BOARD_DEFINITIONS_H
--- SensorHub_FW_CMake/src/main.c
+++ SensorHub_FW_CMake/src/main.c
@@ -16,16 +16,67 @@ an array of StackType_t variables.  The size of StackType_t is dependent on
 the RTOS port. */
 StackType_t xStack[ STACK_SIZE ];
 
+const unsigned char testbuf[30][3] ={{0x02, 0x07,0x01},
+                                     {0x02, 0x08,0x01},
+                                     {0x00, 0x96,0x00},
+                                     {0x00, 0x97, 0xfd},
+                                     {0x00, 0xe3, 0x00},
+                                     {0x00, 0xe4, 0x04},
+                                     {0x00, 0xe5, 0x02},
+                                     {0x00, 0xe6, 0x01},
+                                     {0x00, 0xe7, 0x03},
+                                     {0x00, 0xf5, 0x02},
+                                     {0x00, 0xd9, 0x05},
+                                     {0x00, 0xdb, 0xce},
+                                     {0x00, 0xdc, 0x03},
+                                     {0x00, 0xdd, 0xf8},
+                                     {0x00, 0x9f, 0x00},
+                                     {0x00, 0xa3, 0x3c},
+                                     {0x00, 0xb7, 0x00},
+                                     {0x00, 0xbb, 0x3c},
+                                     {0x00, 0xb2, 0x09},
+                                     {0x00, 0xca, 0x09},
+                                     {0x01, 0x98, 0x01},
+                                     {0x01, 0xb0, 0x17},
+                                     {0x01, 0xad, 0x00},
+                                     {0x00, 0xff, 0x05},
+                                     {0x01, 0x00, 0x05},
+                                     {0x01, 0x99, 0x05},
+                                     {0x01, 0xa6, 0x1b},
+                                     {0x01, 0xac, 0x3e},
+                                     {0x01, 0xa7, 0x1f},
+                                     {0x00, 0x30, 0x00}};
 
+//const unsigned char resetreg[3] = {0x00, 0x16, 0xB4};
+const unsigned char StartMeasurement[3] = {0x00, 0x18, 0x01};
+const unsigned char InterruptClear[3] = {0x00, 0x15, 0x07};
+const unsigned char ReadRange[2] = {0x00, 0x62};
+//static const unsigned char idreg[3] = {0x00, 0x00, 0xB4};
+static uint8_t data;
 /* Function that implements the task being created. */
 void vTaskCode( void * pvParameters )
 {
     /* The parameter value is expected to be 1 as 1 is passed in the
        pvParameters value in the call to xTaskCreateStatic(). */
+//    for (uint8_t i =0; i < 30; i++) {
+//        i2c_write_blocking(&I2CPeriph, 0x29, testbuf[i], 3, 0);
+//    }
+//    i2c_write_blocking(&I2CPeriph, 0x29, resetreg, 2, 1);
+    vTaskDelay(10/portTICK_RATE_MS);
+    for(uint8_t i=0; i< 30; i++) {
+        i2c_write_blocking(&I2CPeriph, 0x29, testbuf[i], 3, 1);
+//        vTaskDelay(1/portTICK_RATE_MS);
+ //       i2c_read_blocking(&I2CPeriph, 0x29, data, 1);
+        //vTaskDelay(1/portTICK_RATE_MS);
+    }
     for( ;; )
     {
+        i2c_write_blocking(&I2CPeriph, 0x29, StartMeasurement, 3, 1);
         ToggleGPIOPin(LedPin);
-        vTaskDelay(50/portTICK_PERIOD_MS);
+        vTaskDelay(10/portTICK_PERIOD_MS);
+        i2c_write_blocking(&I2CPeriph, 0x29, InterruptClear, 3, 1);
+        i2c_write_blocking(&I2CPeriph, 0x29, ReadRange, 2, 1);
+        i2c_read_blocking(&I2CPeriph, 0x29, &data, 1);
     }
 }
 
@@ -77,6 +128,8 @@ void i2c_slave_stop_irq(const void *const hw) {
     CurrTransaction.crc = 0;
     ((Sercom*)hw)->I2CS.INTFLAG.reg = SERCOM_I2CS_INTFLAG_PREC;
 }
+
+
 int main(void)
 {
     /*
@@ -94,7 +147,7 @@ int main(void)
             tskIDLE_PRIORITY,/* Priority at which the task is created. */
             xStack,          /* Array to use as the task's stack. */
             &xTaskBuffer );  /* Variable to hold the task's data structure. */
-    //i2c_init(&I2CPeriph, I2C_CLOCK_SPEED);
+    i2c_init(&I2CPeriph, I2C_CLOCK_SPEED);
     SetGPIOPinFunction(kBackBoneSCL, kGPIOFunctionD);
     SetGPIOPinFunction(kBackBoneSDA, kGPIOFunctionD);
     vTaskStartScheduler();
