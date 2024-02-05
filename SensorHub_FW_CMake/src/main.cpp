f0bf12789dad5c6ff0e37b0e70d5b7ac64c083ef
Johan <johan@robotpatient.com>
2024-02-05 10:35:26 +0100

    -enh Added comments.

--- SensorHub_FW_CMake/src/main.cpp
+++ SensorHub_FW_CMake/src/main.cpp
@@ -30,8 +30,8 @@ an array of StackType_t variables.  The size of StackType_t is dependent on
 the RTOS port. */
 StackType_t xStack[ STACK_SIZE ];
 
-I2CDriver sensor_port_b = I2CDriver(I2C_HOST_INST_PORT_A, kI2cSpeed_100KHz);
-I2CDriver sensor_port_a = I2CDriver(I2C_HOST_INST_PORT_B, kI2cSpeed_100KHz);
+I2CDriver sensor_port_b = I2CDriver(I2C_HOST_INST_PORT_B, kI2cSpeed_100KHz);
+I2CDriver sensor_port_a = I2CDriver(I2C_HOST_INST_PORT_A, kI2cSpeed_100KHz);
 I2CSlaveDriver backbone_port = I2CSlaveDriver(I2C_SLAVE_INST_BACKBONE_PORT);
 
 CompressionSensor compressionSensor;
@@ -52,20 +52,23 @@ UniversalSensor* ConnectedSensor_PortB[MAX_SENSORS_PER_PORT] = {};
 /* Function that implements the task being created. */
 void vTaskCode( void * pvParameters )
 {
-    for( ;; )
-    {
+  for( ;; )
+  {
+    // Note: always check if sensors are available as they are maybe not initialized yet or have become unavailable during operations
 
-        if (positioningSensor.Available()) {
-          sensorData = positioningSensor.GetSensorData();
-        }
-        /*
-        if (compressionSensor.Available()) {
-          sensorData = compressionSensor.GetSensorData();
-        }*/
-        
-        vTaskDelay(100/portTICK_PERIOD_MS);
+    if (positioningSensor.Available()) {
+      sensorData = positioningSensor.GetSensorData();
     }
+    /*
+    if (compressionSensor.Available()) {
+      sensorData = compressionSensor.GetSensorData();
+    }*/
+
+    vTaskDelay(100/portTICK_PERIOD_MS);
+  }
 }
+
+
 
 /**
  * @brief Method to initialize the sensors that are connected to the SensorHub.
@@ -91,52 +94,70 @@ void Init_backbone() {
  */
 
 void Init_sensors() {
-    sensor_port_a.Init();
-    sensor_port_b.Init();
+  sensor_port_a.Init();
+  sensor_port_b.Init();
 
-    // ToDo: implement in such a way that the sensor is detected or not per port.
-    //compressionSensor.Initialize(&sensor_port_a);
-    compressionSensor.Initialize(&sensor_port_b);
-    positioningSensor.Initialize(&sensor_port_b);
-
-    /*
-    if (!compressionSensor.Available()) {
-      compressionSensor.Initialize(&sensor_port_b);
-    }*/
+  // ToDo: implement in such a way that the sensor is detected or not per port.
+  // compressionSensor.Initialize(&sensor_port_a);
+  compressionSensor.Initialize(&sensor_port_b);
+  positioningSensor.Initialize(&sensor_port_b);
 }
+
+/**
+ * @brief Method to initialize the system, scheduled using FreeRTOS.
+ * Initializes:
+ *  - clock(s)
+ *  - IO-pins
+ *  - communication
+ *  - sensors
+ * @param none.
+ * @return void
+ */
+
+void systemInitTask( void * pvParameters ) {
+  Clock_Init();
+  Init_pins();
+  Init_backbone();
+  Init_sensors();
+
+  backbone_port.set_external_register_buffer(&public_reg);
+  setup_evsys_handler();
+  backbone_port.force_update_internal_buffer(public_reg.STATUS, 2);
+}
+
+/**
+ * @brief Method to initialize the FreeRTOS scheduler.
+ * creates two tasks:
+ * - systemInitTask to initialize the system
+ * - standard task to read the sensors etc.
+ * @param none.
+ * @return void
+ */
+
+void InitScheduler() {
+  xTaskCreate(systemInitTask, "SYSINIT", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2, NULL);
+  xTaskCreateStatic(
+          vTaskCode,       /* Function that implements the task. */
+          "CBTASK",          /* Text name for the task. */
+          STACK_SIZE,      /* Number of indexes in the xStack array. */
+          ( void * ) 1,    /* Parameter passed into the task. */
+          tskIDLE_PRIORITY,/* Priority at which the task is created. */
+          xStack,          /* Array to use as the task's stack. */
+          &xTaskBuffer );  /* Variable to hold the task's data structure. */
+
+  vTaskStartScheduler();
+}
+
 
 /** Same documentation for both members. Details can be added here. */
 
 int main(void)
 {
-    /*
-     * Set the main clock to 48MHz
-     */
-    Clock_Init();
-    Init_pins();
-    Init_backbone();
-    Init_sensors();
 
-    //positioningSensor.SensorTest();
+  InitScheduler();
 
-    backbone_port.set_external_register_buffer(&public_reg);
-
-    setup_evsys_handler();
-
-    backbone_port.force_update_internal_buffer(public_reg.STATUS, 2);
-
-    xTaskCreateStatic(
-            vTaskCode,       /* Function that implements the task. */
-            "CBTASK",          /* Text name for the task. */
-            STACK_SIZE,      /* Number of indexes in the xStack array. */
-            ( void * ) 1,    /* Parameter passed into the task. */
-            tskIDLE_PRIORITY,/* Priority at which the task is created. */
-            xStack,          /* Array to use as the task's stack. */
-            &xTaskBuffer );  /* Variable to hold the task's data structure. */
-
-    vTaskStartScheduler();
-    while (1) {
-    }
+  while (1) {
+  }
 }
 
 
